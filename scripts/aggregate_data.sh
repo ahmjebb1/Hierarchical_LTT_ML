ec954fc291444820d02c543a229738058215926d
ahmjebb1 <a.h.m.jebb@gmail.com>
2026-06-18 13:42:29 +0100

    Additional notes on function of aggregate_data.sh

--- scripts/aggregate_data.sh
+++ scripts/aggregate_data.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+#loops over every file in the current directory
+#takes file name x extracts the first five characters and stores as exp
+#reads file and takes only lines containing "Test-set"
+#the whole output produces a CSV-style summary across all experiment runs
 for x in `ls` ; do exp=`echo $x | cut -c1-5` ; cat $x | grep "Test-set" | awk -v fn="$exp" '{print fn","$4","$6}' ; done
 
