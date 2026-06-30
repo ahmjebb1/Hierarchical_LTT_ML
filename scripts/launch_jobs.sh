21c90771460dd6fa2e2847e209171283af4ee953
ahmjebb1 <a.h.m.jebb@gmail.com>
2026-06-30 09:44:32 +0100

    Removal of multiple class sizes on launch jobs

--- scripts/launch_jobs.sh
+++ scripts/launch_jobs.sh
@@ -3,5 +3,5 @@
 # Assuming we have yamls in yaml/ called $exp.run.yaml, this script
 # will launch HPC jobs that load up each yaml. Useful for running a batch
 # without specifically controlling which paramset to use within the job array.
-for exp in ltt10 ltt20 ltt30 ltt40 ltt50 wwc10 wwc20 wwc30 wwc40 wwc50 ; do sbatch --job-name="$exp" birdcall_hpc.sh yaml/$exp.run.yaml ; done
+for exp in ltt10 ltt20 ; do sbatch --job-name="$exp" birdcall_hpc.sh yaml/$exp.run.yaml ; done
 
